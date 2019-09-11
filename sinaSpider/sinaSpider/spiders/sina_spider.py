f96454b664ab574a4acea57da47631a9c6851067
renxiulong <77759580@qq.com>
2019-09-11 17:58:58 +0800

    sina spider response.css split

--- sinaSpider/sinaSpider/spiders/sina_spider.py
+++ sinaSpider/sinaSpider/spiders/sina_spider.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import scrapy
@@ -13,9 +13,10 @@ class SinaSpider(scrapy.Spider):
 	# https://feed.sina.com.cn/api/roll/get?pageid=107&lid=1244&num=5&versionNumber=1.2.4&page=1&encode=utf-8&callback=feedCardJsonpCallback&_=1567144901827
 	urls = []
 	titles = []
-	start_flag = False
+	start_flag = True
 
 	def parse(self, response):
+		print('self.start_flag=', self.start_flag)
 		if self.start_flag:
 			jsonobj = json.loads(response.body.decode('utf-8'))
 			result = jsonobj['result']
@@ -26,6 +27,25 @@ class SinaSpider(scrapy.Spider):
 
 			print(self.titles)
 			print(self.urls)
-			self.start_flag = True
+			self.start_flag = False
+			next_page = self.urls[0]
+			if next_page is not None:
+				next_page = 'http://slide.ent.sina.com.cn/star/slide_4_704_321623.html'
+				yield scrapy.Request(next_page, self.parse)
 		else:
+			contents = response.css('div#eData>dl>dd::text').extract()
+			rows = len(contents)
+			for num in range(0, rows, 6):
+				print('url:%s' % contents[num])
+				print('content:%s' % contents[num + 4])
 			pass
+			# print(response.text)
+			# with open('sina.txt', 'wb+') as fs:
+			#	fs.write(response.text.encode('utf-8'))
+			# with open('content.txt', 'wb+') as f:
+			#	title = response.css('h1.main-title::text').extract_first()
+			#	print('title:%s' % title)
+			#	content = response.css('div.quotation>p::text').extract_first()
+			#	print('content:%s' % content)
+			#	f.write(title.encode('utf-8'))
+			#	f.write(content.encode('utf-8'))
